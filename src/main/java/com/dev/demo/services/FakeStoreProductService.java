65b3546b442604603f02ed033108f468a80a7c7e
Amitoshverm <amitosh.verma9@gmail.com>
2025-11-21 00:39:59 +0530

    restTemplate and first api call

--- src/main/java/com/dev/demo/services/FakeStoreProductService.java
+++ src/main/java/com/dev/demo/services/FakeStoreProductService.java
@@ -1,16 +1,39 @@
 package com.dev.demo.services;
 
+import com.dev.demo.dtos.FakeStoreProductDto;
+import com.dev.demo.dtos.GenericProductDto;
 import com.dev.demo.models.Product;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
-@Service("fakeProductService")
+@Service("fakeStoreProductService")
 public class FakeStoreProductService implements ProductService{
 
+    private RestTemplateBuilder restTemplateBuilder;
+    private String getRequestUrl = "https://fakestoreapi.com/products/{id}";
+
+    public FakeStoreProductService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplateBuilder = restTemplateBuilder;
+    }
+
     @Override
-    public Product getProductById(Long id) {
-        return null;
+    public GenericProductDto getProductById(Long id) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        ResponseEntity<FakeStoreProductDto> response= restTemplate.getForEntity(getRequestUrl, FakeStoreProductDto.class, id);
+//        response.getStatusCode();
+        FakeStoreProductDto fakeStoreProductDto = response.getBody();
+
+        GenericProductDto product = new GenericProductDto();
+        product.setTitle(fakeStoreProductDto.getTitle());
+        product.setDescription(fakeStoreProductDto.getDescription());
+        product.setPrice(fakeStoreProductDto.getPrice());
+        product.setCategory(fakeStoreProductDto.getCategory());
+        product.setImage(fakeStoreProductDto.getImage());
+        return product;
     }
 
     @Override
