119f556f5b634ead28d5bfda2db7cf7eb8552904
Amitoshverm <amitosh.verma9@gmail.com>
2025-11-21 00:38:10 +0530

    dtos

--- src/main/java/com/dev/demo/dtos/GenericProductDto.java
+++ src/main/java/com/dev/demo/dtos/GenericProductDto.java
@@ -1,4 +1,14 @@
 package com.dev.demo.dtos;
 
+import lombok.Data;
+
+@Data
 public class GenericProductDto {
+//    private Long id;
+    private String title;
+    private double price;
+    private String description;
+    private String category;
+    private String image;
+
 }
