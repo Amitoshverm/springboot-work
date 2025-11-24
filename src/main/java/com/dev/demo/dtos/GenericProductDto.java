73c40c4a1237ecb3f0ac24e0bdcb92ca28205449
Amitoshverm <amitosh.verma9@gmail.com>
2025-11-25 03:01:29 +0530

    added id field

--- src/main/java/com/dev/demo/dtos/GenericProductDto.java
+++ src/main/java/com/dev/demo/dtos/GenericProductDto.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class GenericProductDto {
-//    private Long id;
+    private Long id;
     private String title;
     private double price;
     private String description;
