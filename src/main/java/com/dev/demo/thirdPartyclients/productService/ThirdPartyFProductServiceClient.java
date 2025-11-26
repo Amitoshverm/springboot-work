954b289100b0f05e8219c78ed810a7056130c222
Amitoshverm <amitosh.verma9@gmail.com>
2025-11-26 23:24:16 +0530

    modularise for third party api

---
+++ src/main/java/com/dev/demo/thirdPartyclients/productService/ThirdPartyFProductServiceClient.java
@@ -0,0 +1,4 @@
+package com.dev.demo.thirdPartyclients.productService;
+
+public interface ThirdPartyFProductServiceClient {
+}
---
+++ src/main/java/com/dev/demo/thirdPartyclients/productService/fakeStore/FakeStoreProductServiceClient.java
@@ -0,0 +1,4 @@
+package com.dev.demo.thirdPartyclients.productService.fakeStore;
+
+public class FakeStoreProductServiceClient {
+}
