8ca66f4fc6f5e5d8c552de8e8cd7c4280df052e0
Amitoshverm <amitosh.verma9@gmail.com>
2025-12-17 01:00:26 +0530

    @entity

--- src/main/java/com/dev/demo/models/Category.java
+++ src/main/java/com/dev/demo/models/Category.java
@@ -1,5 +1,8 @@
 package com.dev.demo.models;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Category extends BaseModel{
     private String name;
 }
--- src/main/java/com/dev/demo/models/Product.java
+++ src/main/java/com/dev/demo/models/Product.java
@@ -1,8 +1,10 @@
 package com.dev.demo.models;
 
+import jakarta.persistence.Entity;
 import lombok.Data;
 
 @Data
+@Entity
 public class Product extends BaseModel{
     private String title;
     private String description;
