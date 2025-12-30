c84b1244d642c248f7131d991d1dd9d7a38da5af
Amitoshverm <amitosh.verma9@gmail.com>
2025-12-30 23:20:36 +0530

    uuid generator

--- src/main/java/com/dev/demo/models/BaseModel.java
+++ src/main/java/com/dev/demo/models/BaseModel.java
@@ -1,10 +1,18 @@
 package com.dev.demo.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
 
 @MappedSuperclass
 public class BaseModel {
     @Id
-    private Long id;
+    @GeneratedValue(generator = "amitosh")
+    @GenericGenerator(name="amitosh", strategy = "uuid2")
+    @Column(name="id", columnDefinition = "binary(16)", nullable = false)
+    private UUID id;
 }
